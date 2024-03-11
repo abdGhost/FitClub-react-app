efc0272f6b91fbddbc64b91793a9a5fcd9c5b1ab
Abdul Taher Khan <49634197+abdGhost@users.noreply.github.com>
2024-03-11 18:06:01 +0530

    working on hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,6 +7,22 @@ const Hero = () => {
     <div className="hero">
       <div className="left-h">
         <Header />
+        {/* The best ad */}
+        <div className="the-best-ad">
+          <div></div>
+          <span>the best fitness club in the town</span>
+        </div>
+
+        {/* Hero heading */}
+        <div className="hero-text">
+          <div>
+            <span>Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal Body</span>
+          </div>
+        </div>
       </div>
       <div className="right-h">Right side</div>
     </div>
