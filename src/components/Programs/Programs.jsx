c609ce0d58d51eb705d36b1408bc1a9dea87fbfd
Abdul Taher Khan <49634197+abdGhost@users.noreply.github.com>
2024-03-13 17:15:01 +0530

    Worked on Programs component

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./Programs.css";
 
 const Programs = () => {
-  return <div>Programs</div>;
+  return (
+    <div className="Progress" id="progress">
+      {/* Header */}
+      <div className="programs-header">
+        <spna className="stroke-text">Explore our</spna>
+        <spna>Programs</spna>
+        <spna className="stroke-text">to shape you</spna>
+      </div>
+    </div>
+  );
 };
 
 export default Programs;
