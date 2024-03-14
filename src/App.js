d9e970b67be8ee5e8b4014c08604d8ac6714f184
Abdul Taher Khan <49634197+abdGhost@users.noreply.github.com>
2024-03-14 14:28:04 +0530

    added reasons component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero/Hero.jsx";
 import Programs from "./components/Programs/Programs.jsx";
+import Header from "./components/Reasons/Reasons.jsx";
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <Programs />
+      <Header />
     </div>
   );
 }
