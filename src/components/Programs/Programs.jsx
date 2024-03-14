05a7250cef25f0d35b395a4ad6ea60e34506e2a2
Abdul Taher Khan <49634197+abdGhost@users.noreply.github.com>
2024-03-14 10:35:50 +0530

    worked on Programs component

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Programs.css";
+import { programsData } from "../../data/programsData";
+import RightArrow from "../../assets/rightArrow.png";
 
 const Programs = () => {
   return (
@@ -9,6 +11,21 @@ const Programs = () => {
         <spna className="stroke-text">Explore our</spna>
         <spna>Programs</spna>
         <spna className="stroke-text">to shape you</spna>
+      </div>
+
+      <div className="program-categories">
+        {programsData.map((program) => (
+          <div className="category">
+            {program.image}
+            <spna>{program.heading}</spna>
+            <spna>{program.details}</spna>
+
+            <div className="join-now">
+              <spna>Join Now</spna>
+              <img src={RightArrow} alt="" />
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
